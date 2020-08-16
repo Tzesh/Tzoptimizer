6d278e402cf1c2828e8eb063e046328f66e5f4e1
Uğur Dindar <55401127+Tzesh@users.noreply.github.com>
2020-08-16 16:00:14 +0300

    Tzoptimizer v1.3

--- Program.cs
+++ Program.cs
@@ -18,7 +18,7 @@ namespace Windows_Optimizer
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Tzoptimizer());
         }
     }
 }
